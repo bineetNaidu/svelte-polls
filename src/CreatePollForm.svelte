b2257600dd88672ea83d139b0e77d568782833ba
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-10 16:20:59 +0530

    add form validations

--- src/CreatePollForm.svelte
+++ src/CreatePollForm.svelte
@@ -2,8 +2,38 @@
   import Button from './Button.svelte';
 
   let field = { question: '', answerA: '', answerB: '' };
+  let errors = { question: '', answerA: '', answerB: '' };
+  let valid = false;
 
-  const handleSUbmit = () => console.log(field);
+  const handleSubmit = () => {
+    valid = true;
+    // validate question
+    if (field.question.trim().length < 5) {
+      valid = false;
+      errors.question = 'Question must be at least 5 character long';
+    } else {
+      errors.question = '';
+    }
+    // validate answer A
+    if (field.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = 'answerA must be at least 1 character long';
+    } else {
+      errors.answerA = '';
+    }
+    // validate answer A
+    if (field.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = 'answerB must be at least 1 character long';
+    } else {
+      errors.answerB = '';
+    }
+
+    // add new poll
+    if (valid) {
+      console.log(field);
+    }
+  };
 </script>
 
 <style>
@@ -26,22 +56,31 @@
     margin: 10px auto;
     text-align: left;
   }
+
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
+  }
 </style>
 
-<form on:submit|preventDefault={handleSUbmit}>
+<form on:submit|preventDefault={handleSubmit}>
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={field.question} />
+    <div class="error">{errors.question}</div>
   </div>
 
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={field.answerA} />
+    <div class="error">{errors.answerA}</div>
   </div>
 
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={field.answerB} />
+    <div class="error">{errors.answerB}</div>
   </div>
 
   <Button type="secondary" flat>Add Poll</Button>
