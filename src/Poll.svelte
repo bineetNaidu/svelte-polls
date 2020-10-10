cabcfcf5218aa1a0fa5b7fd5e3386340af8fb3e7
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-10 21:26:08 +0530

    add votes bar W/ dynamic values

--- src/Poll.svelte
+++ src/Poll.svelte
@@ -8,6 +8,8 @@
 
   // reactive value
   $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
 
   // Functions
 
@@ -38,6 +40,20 @@
     display: inline-block;
     padding: 10px 20px;
   }
+
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+
+  .percent-a {
+    background: rgba(217, 27, 66, 0.2);
+  }
+
+  .percent-b {
+    background: rgba(69, 196, 150, 0.2);
+  }
 </style>
 
 <Card>
@@ -45,11 +61,11 @@
     <h3>{poll.question}</h3>
     <p>{totalVotes}</p>
     <div class="answer" on:click={() => handleVote('a', poll.id)}>
-      <div class="percent percent-a" />
+      <div class="percent percent-a" style="width: {percentA}%" />
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
-      <div class="percent percent-b" />
+      <div class="percent percent-b" style="width: {percentB}%" />
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
